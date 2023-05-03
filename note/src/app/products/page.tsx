f473896a7f124cd6187af1790808b7aa9b986830
Topbin2 <mosangbin94@gmail.com>
2023-05-03 19:01:58 +0900

    wip

--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -1,21 +1,19 @@
+import { getProducts } from "@/service/products";
 import Link from "next/link";
 import React from "react";
 
-const PRODUCTS = [
-  { id: 1, name: "shirt" },
-  { id: 2, name: "pants" },
-  { id: 3, name: "skirt" },
-  { id: 4, name: "shoes" },
-] as const;
+const products = ["shirt", "pants", "skirt", "shoes"] as const;
 
 const ProductsPage = () => {
+  // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
+  const products = getProducts();
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <h1>제품 소개 페이지!</h1>
       <ul>
-        {PRODUCTS.map((product) => (
-          <li key={product.id}>
-            <Link href={`/products/${product.name}`}>{product.name}</Link>
+        {products.map((product, index) => (
+          <li key={index}>
+            <Link href={`/products/${product}`}>{product}</Link>
           </li>
         ))}
       </ul>
