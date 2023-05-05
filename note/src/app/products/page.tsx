69a89c415a847ed07e59ee9b7b6348ef37228f2d
MoSangBin <mosangbin94@gmail.com>
2023-05-05 16:58:52 +0900

    ssg

--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -2,18 +2,16 @@ import { getProducts } from "@/service/products";
 import Link from "next/link";
 import React from "react";
 
-const products = ["shirt", "pants", "skirt", "shoes"] as const;
-
-const ProductsPage = () => {
+const ProductsPage = async () => {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
-  const products = getProducts();
+  const products = await getProducts();
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <h1>제품 소개 페이지!</h1>
       <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            <Link href={`/products/${product}`}>{product}</Link>
+        {products.map(({ id, name }) => (
+          <li key={id}>
+            <Link href={`/products/${id}`}>{name}</Link>
           </li>
         ))}
       </ul>
