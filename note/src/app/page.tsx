b9f36a39e32805d48f9ab5cc84dc33f1b7271c47
Topbin2 <mosangbin94@gmail.com>
2023-05-08 12:04:11 +0900

    image

--- note/src/app/page.tsx
+++ note/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Counter from "@/components/Counter";
 import os from "os";
+import Image from "next/image";
 
 export default function Home() {
   console.log("안녕! - 서버");
@@ -9,6 +10,12 @@ export default function Home() {
     <>
       <h1>홈페이지</h1>
       <Counter />
+      <Image
+        src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
+        alt="shop"
+        width={400}
+        height={400}
+      />
     </>
   );
 }
--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -1,17 +1,19 @@
 import MeowArticle from "@/components/MeowArticle";
 import { getProducts } from "@/service/products";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import clothesImage from "public/images/clothes.jpg";
 
 // export const revalidate = 3;
 
 export default async function ProductsPage() {
-  throw new Error();
   const products = await getProducts();
 
   return (
     <>
       <h1>제품 소개 페이지!</h1>
+      <Image src={clothesImage} alt="Clothes" priority />
       <ul>
         {products.map(({ id, name }) => (
           <li key={id}>
